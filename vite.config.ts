59da6bb404592252443203162fcd8a602d2cd4f4
dangerfireon13 <rotinaagro549@gmail.com>
2026-02-20 21:26:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ const __dirname = path.dirname(__filename);
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: './',
         
       server: {
         port: 3000,
